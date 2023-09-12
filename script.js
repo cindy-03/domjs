823df174d9ecda6db92300fd589760b1fb23c752
cindy-03 <143466905+cindy-03@users.noreply.github.com>
2023-09-12 09:09:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const judul = document.getElementById('judul');
+judul.style.color='white';
+judul.style.backgroundColor="black";
+judul.innerHTML="Cindy"
+//document.getElementsByTagName()
+const p = document.getElementsByTagName('p');
+p[0].style.backgroundColor='blue';
+p[1].style.backgroundColor='blue';
+p[2].style.backgroundColor='blue';
+p[3].style.backgroundColor='blue';
+const h1 = document.getElementsByTagName('h1');
+ h1[0].style.fontSize='50px';
+ const p1 = document.querySelector('p');
+ p1.innerHTML = "dirubah dengan javascript"; 
+//document.querySelector()
+const p4 = document.querySelector('#b p');
+p4.style.color ='green';
+p4.style.fontSize ='25px';
